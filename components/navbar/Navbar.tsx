e2d5048f900672e70ed3df5fe6aafd770c99292d
Radu Bordea <radu.bordea.dev@gmai.com>
2026-01-14 15:38:01 +0100

    create navbar component setup

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,6 +1,24 @@
+import Container from "../global/Container";
+import CartButton from "./CartButton";
+import DarkMode from "./DarkMode";
+import LinksDropdown from "./LinksDropdown";
+import Logo from "./Logo";
+import NavSearch from "./NavSearch";
+
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div className="border-b">
+      <Container className="flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap py-8">
+        <Logo />
+        <NavSearch />
+        <div className="flex gap-4 items-center">
+          {/* Future nav items can go here */}
+          <CartButton />
+          <DarkMode />
+          <LinksDropdown />
+        </div>
+      </Container>
+    </div>
+  );
 }
-export default Navbar
+export default Navbar;
