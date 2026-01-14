2bb1525cc976c6225e8d33158178c63664c375ab
Radu Bordea <radu.bordea.dev@gmai.com>
2026-01-14 15:46:08 +0100

    create logo component

--- README.md
+++ README.md
@@ -10,3 +10,6 @@
 - npx shadcn@latest add input label popover 
 - npx shadcn@latest add select separator table 
 - npx shadcn@latest add textarea sonner skeleton carousel
+
+# libraries
+- npm install react-icons
--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,6 +1,14 @@
+import { ImHappy } from "react-icons/im";
+import { Button } from "../ui/button";
+import Link from "next/link";
+
 function Logo() {
   return (
-    <div>Logo</div>
+    <Button asChild variant="outline" className="rounded-full px-3 py-1" size="icon">
+      <Link href="/">
+        <ImHappy className="h-4 w-4 text-cyan-600" />
+      </Link>
+    </Button>
   )
 }
 export default Logo
