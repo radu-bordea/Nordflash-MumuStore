8e45a16ca2008c4599b7898b8b0c38ffc8875021
Radu Bordea <radu.bordea.dev@gmai.com>
2026-01-16 15:07:10 +0100

    creatre grid products component and fetch products

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import FeaturedProducts from "@/components/home/FeaturedProducts";
 import Hero from "@/components/home/Hero";
-import { Button } from "@/components/ui/button";
 
 function HomePage() {
   return (
