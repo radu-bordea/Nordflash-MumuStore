26db9185c12c579aa548eafab1d51275dfbf3b55
Radu Bordea <radu.bordea.dev@gmai.com>
2026-01-16 14:32:09 +0100

    create section title and emptylist components

--- components/global/SectionTitle.tsx
+++ components/global/SectionTitle.tsx
@@ -1,6 +1,13 @@
-const SectionTitle = () => {
+import { Separator } from "@/components/ui/separator";
+
+const SectionTitle = ({ text }: { text: string }) => {
   return (
-    <div>SectionTitle</div>
-  )
-}
-export default SectionTitle
+    <div>
+      <h2 className="text-3xl font-medium tracking-wider capitalize mb-8">
+        {text}
+      </h2>
+      <Separator />
+    </div>
+  );
+};
+export default SectionTitle;
