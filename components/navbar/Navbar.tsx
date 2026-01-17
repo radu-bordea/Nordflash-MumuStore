7415996d17f0a9de916ca99574ca7e5462dc5b0d
Radu Bordea <radu.bordea.dev@gmai.com>
2026-01-17 14:20:00 +0100

    about page

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import Container from "../global/Container";
 import CartButton from "./CartButton";
-import {DarkMode} from "./DarkMode";
+import { DarkMode } from "./DarkMode";
 import LinksDropdown from "./LinksDropdown";
 import Logo from "./Logo";
 import NavSearch from "./NavSearch";
@@ -8,10 +8,12 @@ import NavSearch from "./NavSearch";
 function Navbar() {
   return (
     <div className="border-b">
-      <Container className="flex flex-col sm:flex-row  sm:justify-between sm:items-center flex-wrap gap-6 py-8">
-        <Logo />
-        <NavSearch />
-        <div className="flex gap-4 items-center">
+      <Container className="flex flex-col-reverse sm:flex-row sm:justify-between sm:items-center flex-wrap gap-6 py-8">
+        <div className="flex gap-4 justify-center">
+          <Logo />
+          <NavSearch />
+        </div>
+        <div className="flex gap-4 items-center justify-end">
           {/* Future nav items can go here */}
           <CartButton />
           <DarkMode />
