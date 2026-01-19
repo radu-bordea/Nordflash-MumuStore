de6e356ba2a2272dcd6951a84d92b09a653cba6b
radu-bordea <radu.bordea.dev@gmail.com>
2026-01-19 19:30:12 +0100

    add toaster provider

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import { ThemeProvider } from "./theme-provider";
+import { Toaster } from "sonner";
 
 function providers({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <Toaster />
       <ThemeProvider
         attribute="class"
         defaultTheme="system"
