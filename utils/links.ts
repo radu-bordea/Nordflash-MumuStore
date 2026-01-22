a972c862235d0e99a467435a21beef141807f386
Radu Bordea <radu.bordea.dev@gmai.com>
2026-01-22 14:10:04 +0100

    create adminLinks

--- utils/links.ts
+++ utils/links.ts
@@ -4,10 +4,17 @@ type NavLink = {
 };
 
 export const links: NavLink[] = [
-  { href: '/', label: 'home' },
-  { href: '/about', label: 'about' },
-  { href: '/products', label: 'products' },
-  { href: '/favorites', label: 'favorites' },
-  { href: '/cart', label: 'cart' },
-  { href: '/orders', label: 'orders' },
+  { href: "/", label: "home" },
+  { href: "/about", label: "about" },
+  { href: "/products", label: "products" },
+  { href: "/favorites", label: "favorites" },
+  { href: "/cart", label: "cart" },
+  { href: "/orders", label: "orders" },
+  { href: "/admin/sales", label: "dashboard" },
+];
+
+export const adminLinks: NavLink[] = [
+  { href: "/admin/sales", label: "sales" },
+  { href: "/admin/products", label: "my products" },
+  { href: "/admin/products/create", label: "create product" },
 ];
