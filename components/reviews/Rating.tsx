60d589cb96184b945110430dbcccb4c66180dfa3
Radu Bordea <radu.bordea.dev@gmai.com>
2026-02-05 16:05:37 +0100

    create Rating component

--- components/reviews/Rating.tsx
+++ components/reviews/Rating.tsx
@@ -1,6 +1,25 @@
-function Rating() {
+import { FaStar, FaRegStar } from "react-icons/fa";
+
+function Rating({ rating }: { rating: number }) {
+  // rating = 2
+  // 1 <= 2 true
+  // 2 <= 2 true
+  // 3 <= 2 false
+  // 4 <= 2 false
+  // 5 <= 2 false
+  const stars = Array.from({ length: 5 }, (_, i) => i + 1 <= rating);
+
   return (
-    <div>Rating</div>
-  )
+    <div className="flex items-centre gap-x-1">
+      {stars.map((isField, i) => {
+        const className = `w-3 h-3 ${isField ? "text-primary" : "text-gray"}`;
+        return isField ? (
+          <FaStar className={className} key={i} />
+        ) : (
+          <FaRegStar className={className} key={i} />
+        );
+      })}
+    </div>
+  );
 }
-export default Rating
+export default Rating;
