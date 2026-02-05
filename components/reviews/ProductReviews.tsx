8a972c8b79bea8d0764dfc59da1b7b9f0a5a9cf4
Radu Bordea <radu.bordea.dev@gmai.com>
2026-02-05 16:28:11 +0100

    complete ReviewCard and ProductReviews components

--- components/reviews/ProductReviews.tsx
+++ components/reviews/ProductReviews.tsx
@@ -1,6 +1,26 @@
-function ProductReviews() {
+import { fetchProductReviews } from "@/utils/actions";
+import SectionTitle from "../global/SectionTitle";
+import ReviewCard from "./ReviewCard";
+
+async function ProductReviews({ productId }: { productId: string }) {
+  const reviews = await fetchProductReviews(productId);
+
   return (
-    <div>ProductReviews</div>
-  )
+    <div className="mt-16">
+      <SectionTitle text="product reviews" />
+      {reviews.map((review) => {
+        const { comment, rating, authorImageUrl, authorName } = review;
+        const reviewInfo = {
+          comment,
+          rating,
+          image: authorImageUrl,
+          name: authorName,
+        };
+        return (
+          <ReviewCard key={review.id} reviewInfo={reviewInfo}/>
+        );
+      })}
+    </div>
+  );
 }
-export default ProductReviews
+export default ProductReviews;
