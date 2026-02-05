a1ce4ab7e0ab62c80f84469d53774d709ebd5636
Radu Bordea <radu.bordea.dev@gmai.com>
2026-02-05 14:29:27 +0100

    create RatingInput Component

--- components/reviews/SubmitReview.tsx
+++ components/reviews/SubmitReview.tsx
@@ -1,3 +1,14 @@
+'use client';
+import { useState } from 'react';
+import { SubmitButton } from '@/components/form/Buttons';
+import FormContainer from '@/components/form/FormContainer';
+import { Card } from '@/components/ui/card';
+import RatingInput from '@/components/reviews/RatingInput';
+import TextAreaInput from '@/components/form/TextAreaInput';
+import { Button } from '@/components/ui/button';
+import { createReviewAction } from '@/utils/actions';
+import { useUser } from '@clerk/nextjs';
+
 function SubmitReview() {
   return (
     <div>SubmitReview</div>
